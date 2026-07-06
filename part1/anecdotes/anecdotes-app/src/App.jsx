60e374c8dfcf29e64da85373dc2f1033bf89c0d8
caikesn <con.aikens@gmail.com>
2026-07-06 20:39:26 +1000

    anecdotes finished

--- part1/anecdotes/anecdotes-app/src/App.jsx
+++ part1/anecdotes/anecdotes-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 const Button = (props) => {
   return (
     <div>
@@ -7,11 +8,20 @@ const Button = (props) => {
   )
 }
 
+const Favourite = (props) => {
+    return (
+      <div>
+        <h1> {props.text} </h1>
+        <div> {props.value} </div>
+      </div>
+    )
+} 
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
@@ -20,24 +30,33 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(0)
-  
-  const increaseVote = () => {
-    setVotes(votes + 1)
-  }
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const favouriteIndex = votes.indexOf(Math.max(...votes))
+  const favouriteAnecdote = anecdotes[favouriteIndex]
+  const favouriteVoteCount = votes[favouriteIndex]
+
 
   const setToNew = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
+    return randomIndex
   }
+  const increaseVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
 
 
   return (
     <div>
+      <h1> Anecdote of the day! </h1>
       {anecdotes[selected]}
       <Button onClick={setToNew} text={'next anecdote'} />
-      <Button onClick={increaseVote} text={'Votes'} />
-      <div> Votes: {votes}</div>
+      <Button onClick={increaseVote} text={'vote'} />
+      <div>Votes: {votes[selected]}</div>
+      <Favourite value={`${favouriteAnecdote} (votes: ${favouriteVoteCount})`} text={'Anecdote with most votes'} />
     </div>
   )
 }
