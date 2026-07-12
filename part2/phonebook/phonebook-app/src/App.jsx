7fed997e4a8b33fc1fed332c347c94a9c951036b
caikesn <con.aikens@gmail.com>
2026-07-12 18:22:57 +1000

    phonebook done

--- part2/phonebook/phonebook-app/src/App.jsx
+++ part2/phonebook/phonebook-app/src/App.jsx
@@ -1,28 +1,34 @@
 import { useState } from 'react'
 
 
-const Names = (props) => {
-  return (
-    <div> {props.value} </div>
-  )
-}
-const Numbers = (props) => {
-  return (
-    <div> {props.value}</div>
-  )
-}
+const Search = (props) => (
+  <input type="text" value={props.query} onChange={props.onQueryChange} placeholder='Type to search...'/>
+)
 
-const Filter = (props) => {
-  return (
-    <div></div>
-  )
-}
+const PersonForm = (props) => (
+    <form onSubmit={props.onSubmit}>
+      <div>name: <input value={props.newName} onChange={props.onNameChange}/></div>
+      <div>number: <input value={props.newNumber} onChange={props.onNumberChange}/></div>
+      <div><button type="submit">add</button></div> 
+    </form>
+)
 
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map(person => (
+      <div key={person.id}>{person.name} {person.number} </div>
+    ))}
+  </div>
+)
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Connor Aikens', number: '0404 156 789', id: 1 }
-  ]) 
+    { name: 'Connor Aikens', number: '040-1243535', id: 1 },
+    { name: 'Arto Hellas', number: '040-123456', id: 5 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newNumber, setNewNumber] = useState('')
   const [newName, setNewName] = useState('')
   const [showAll, setShowAll] = useState(true)
@@ -64,35 +70,20 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          nunber: <input value={newNumber} onChange={handleNumberChange} />
-        </div>        
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>    
+        <PersonForm
+          newName={newName}
+          newNumber={newNumber}
+          onNameChange={handleNameChange}
+          onNumberChange={handleNumberChange}
+          onSubmit={addPerson}
+        />
       <h2>Names</h2>
-      {persons.map(person => 
-        <div key={person.id}>
-          {person.name} {person.number}
-        </div>
-      )}
+        <Persons persons={persons} />
       <h2>Search for names</h2>
-      <input 
-        type="text" 
-        placeholder='Type to search...'
-        value={query}
-        onChange={(details) => setQuery(details.target.value)}
-      />
-      <ul>
-        {filteredPersons.map((person) => (
-          <li key={person.id}>{person.name} {person.number}</li> 
-        ))}
-      </ul>
+        <Search query={query} onQueryChange={(e) => setQuery(e.target.value)} />
+        <ul>
+          <Persons persons={filteredPersons} />
+        </ul>
     </div>
   )
 }
